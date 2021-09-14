befb3c8c4e12e7223570da765701720650832df9
Dastar Ron <dastar.ron@gmail.com>
2021-09-14 23:24:49 +0300

    wtf

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
 use tictactoe::play;
-<<<<<<< HEAD
 
-//use tictactoe::bot;
-
-fn main() {
-    play::play_game();
-   // bot::foo();
-//    foo();
-=======
 
 fn main() {
     play::play_game();
 
->>>>>>> d937a6942ea4c6561520b4656909f4eb519e3ce2
 }
